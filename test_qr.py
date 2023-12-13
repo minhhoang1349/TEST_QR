35c9e2c158f6df723b898c23e6e2b39a168dfc60
minhhoang1349 <130928649+minhhoang1349@users.noreply.github.com>
2023-12-13 12:52:47 +0000

    Add files via upload

---
+++ test_qr.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+import requests
+import urllib.request
+
+cost=str(input("cost= "))
+
+head_url="https://img.vietqr.io/image/ocb-0004100038288007-HgRYJqu.png?amount="
+tail_url="&addInfo=Thanh%20toan%20he%20thong%20in%20tu%20phuc%20vu&accountName=Nguyen%20Minh%20Hoang"
+
+url=head_url+cost+tail_url
+urllib.request.urlretrieve(url, "local.png")
+
+root=tk.Tk()
+image = tk.PhotoImage(file = "local.png")
+label=tk.Label(root, image=image)
+label.pack()
+def close():
+        label.destroy()
+        button.destroy()
+        root.destroy()
+        
+button=tk.Button(root,text="Payment Completed",command=close)
+button.pack()        
+
+        
+root.mainloop()
